623019f770acb3c63bf999009b8a4b5fa336f520
Tronix <62103846+AxelTronix@users.noreply.github.com>
2021-08-14 23:45:48 +0100

    Updated
    Updated

--- roll.lua
+++ roll.lua
@@ -1,1 +1,6 @@
+Config.Cooldown = 120 --Timer
 
+for i = 0, 3 do
+    StatSetInt(GetHashKey("mp" .. i .. "_shooting_ability"), Config.Cooldown, true)
+    StatSetInt(GetHashKey("sp" .. i .. "_shooting_ability"), Config.Cooldown, true)
+  end
